b1fc2655ff75f3d70f25c29bfdaf39f84a45d3da
zget <dgetahun84@gmail.com>
2018-01-30 20:36:10 -0500

    first commit

---
+++ src/main/java/com/example/demo/SpringBoot10Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot10Application.class, args);
+	}
+}
